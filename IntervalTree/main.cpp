7c186041e62cae4f6c16f8c8e3cb1f0908b5ac83
Aaron Graubert <aarong@vpn5-151.broadinstitute.org>
2017-06-28 10:50:20 -0400

    Initial Commit

---
+++ IntervalTree/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  IntervalTree
+//
+//  Created by Aaron Graubert on 6/28/17.
+//  Copyright © 2017 Aaron Graubert. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
